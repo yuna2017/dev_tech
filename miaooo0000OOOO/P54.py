e8d4bc007c49e189f2447a27f427aea4ab8d6016
miaooo0000OOOO <1683618861@qq.com>
2024-03-12 17:34:09 +0800

    Update P54.py

--- miaooo0000OOOO/P54.py
+++ miaooo0000OOOO/P54.py
@@ -1,4 +1,2 @@
-from math import sqrt
-
-x0, y0, x1, y1 = list(map(float, filter(lambda it: it != "", input().split(" "))))
-print(sqrt((x0 - x1) ** 2 + (y0 - y1) ** 2))
+x0, y0, x1, y1 = map(float, input().split())
+print(((x0 - x1) ** 2 + (y0 - y1) ** 2) ** 0.5)
